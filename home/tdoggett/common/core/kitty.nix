10f5a49c2983e4fd2ed364dbfc5aea3547c80e7f
Tom Doggett <tom@nocoolnametom.com>
2024-08-30 13:53:56 -0400

    Adding swayish keybindings to my hyperland

--- home/tdoggett/common/core/kitty.nix
+++ home/tdoggett/common/core/kitty.nix
@@ -2,6 +2,9 @@
   programs.kitty = {
     enable = true;
     shellIntegration.enableBashIntegration = true;
-    settings = { };
+    settings = {
+      enable_audio_bell = false;
+      visual_bell_duration = "0.1";
+    };
   };
 }
