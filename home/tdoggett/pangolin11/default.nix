fa351d12eaaa288b8950862967c59c045ca03db7
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2024-10-17 12:31:42 -0400

    Hooking in mbp fully

--- home/tdoggett/pangolin11/default.nix
+++ home/tdoggett/pangolin11/default.nix
@@ -17,6 +17,7 @@
     ../common/optional/desktops/hyprland.nix
 
     ############### Service Configurations (Enable below) #################
+    ../common/optional/services/ssh-agent.nix
     ../common/optional/services/gpg-agent.nix
   ];
 
--- hosts/work/macbookpro/default.nix
+++ hosts/work/macbookpro/default.nix
@@ -1,22 +1,40 @@
+###############################################################################
+#
+#  MBP - MBP
+#  Darwin running on MacBook Pro 16-inch 2023 M2
+#
+###############################################################################
+
 {
-  pkgs,
   inputs,
-  configurationRevision ? null,
+  pkgs,
+  lib,
+  configLib,
+  config,
+  configVars,
+  configurationRevision,
   ...
 }:
 {
-  # Auto upgrade nix package and the daemon service.
-  services.nix-daemon.enable = true;
-  # nix.package = pkgs.nix;
+  imports =
+    [
+      ######################## Direct Imports for MBP ############################
+    ]
+    ++ (map configLib.relativeToRoot [
+      #################### Required Configs ####################
+      "hosts/common/darwin"
 
-  # Necessary for using flakes on this system.
-  nix.settings.experimental-features = "nix-command flakes";
+      #################### Host-specific Optional Configs ####################
+      # Be very careful, most of these are meant for NixOS, not Darwin!
+      "hosts/common/optional/direnv.nix"
+      "hosts/common/optional/tmux.nix"
 
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  programs.zsh.enable = true; # default shell on catalina
-  # programs.fish.enable = true;
+      #################### Users to Manage ####################
+      "hosts/common/users/tdoggett/darwin.nix"
+    ]);
 
-  # My Changes from the default template are below
+  networking.hostName = inputs.nix-secrets.networking.work.macbookpro.name;
+
   homebrew.enable = true;
   homebrew.onActivation.autoUpdate = true;
   homebrew.onActivation.cleanup = "uninstall";
@@ -31,60 +49,25 @@
     { name = "homebrew/services"; }
   ];
   environment.systemPackages = [
-    pkgs.silver-searcher # same as ag
     pkgs.awscli2
-    pkgs.dnsmasq
-    pkgs.docutils
     pkgs.kubectl # same as kubernetes-cli
     pkgs.libaom
     pkgs.libass
     pkgs.libassuan
-    pkgs.mkcert
     pkgs.oath-toolkit
     pkgs.openjdk11 # Why do I need version 11? What is this for?
-    # pkgs.openssh # Need to make sure launchctl is switched first!
-    pkgs.tesseract
   ];
 
-  nixpkgs.config.allowUnfree = true;
-
-  nix.gc.automatic = true;
-  nix.optimise.automatic = true;
-
-  environment.shellAliases = {
-    darwin-rebuild-switch = "darwin-rebuild switch --flake ~/.config/nix-darwin";
-    darwin-rebuild-build = "darwin-rebuild build --flake ~/.config/nix-darwin";
-    darwin-rebuild-check = "darwin-rebuild build --flake ~/.config/nix-darwin";
-    rm = "rm -i";
-    cp = "cp -i";
-    ls = "ls -G";
-  };
-
-  programs.direnv.enable = true;
-  programs.direnv.silent = false;
-  programs.tmux.enable = true;
-  programs.tmux.enableMouse = true;
-  programs.tmux.enableSensible = true;
+  # There is no system-level vim management for NixOS, only darwin
+  # TODO: See if I can move this into home-manager instead
   programs.vim.enable = true;
   programs.vim.enableSensible = true;
-  programs.zsh.enableSyntaxHighlighting = true;
 
-  services.dnsmasq.enable = false; # Find out if we want this as a service or if teleport runs it manually
-  services.dnsmasq.addresses =
-    {
-    }
-    // inputs.nix-secrets.networking.workDnsmasq.addresses;
+  services.dnsmasq.enable = false; # TODO Find out if we want this as a service or if teleport runs it manually
 
-  system.defaults.finder.AppleShowAllExtensions = true;
   system.defaults.dock.autohide = false;
   system.defaults.dock.orientation = "right";
   system.defaults.dock.showhidden = true;
-  system.keyboard.enableKeyMapping = true;
-  system.keyboard.remapCapsLockToControl = true;
-  # My Changes from the default template are above
-
-  # Set Git commit hash for darwin-version.
-  system.configurationRevision = configurationRevision;
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
