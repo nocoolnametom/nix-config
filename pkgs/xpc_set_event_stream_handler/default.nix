2b8f82bc0cd22d5af6d9676b56ee5c324a5f744c
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2024-10-17 18:32:22 -0400

    Got Darwin yubikey ssh key switching basically working

---
+++ pkgs/xpc_set_event_stream_handler/default.nix
@@ -0,0 +1,40 @@
+{
+  pkgs ? import <nixpkgs> { },
+  lib ? pkgs.lib,
+  stdenv ? pkgs.stdenv,
+  Foundation ? pkgs.darwin.apple_sdk.frameworks.Foundation,
+  fetchFromGitHub ? pkgs.fetchFromGitHub,
+  xcbuildHook ? pkgs.xcbuildHook,
+}:
+let
+  rev = "4bbfc25b485e444afcca8b9d5492ef0018c03823";
+in
+stdenv.mkDerivation {
+  name = "xpc_set_event_stream_handler";
+  version = builtins.substring 1 7 rev;
+
+  src = fetchFromGitHub {
+    owner = "snosrap";
+    repo = "xpc_set_event_stream_handler";
+    inherit rev;
+    sha256 = "17vv5nacl56h59h3pmawab4cpk54xxg2cxvnijqid4lmvlz6nidq";
+  };
+
+  nativeBuildInputs = [
+    xcbuildHook
+    Foundation
+  ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp Products/Release/xpc_set_event_stream_handler $out/bin
+  '';
+
+  meta = with lib; {
+    description = "Consume a com.apple.iokit.matching event, then run the executable specified in the first parameter.";
+    homepage = "https://github.com/snosrap/xpc_set_event_stream_handler";
+    platforms = platforms.darwin;
+    license = [ licenses.mit ];
+    maintainers = [ maintainers.eqyiel ];
+  };
+}
