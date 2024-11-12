8bede29ba8d00b6448b3f72a6ee962e353b5a09e
Tom Doggett <tom@nocoolnametom.com>
2024-11-12 10:23:43 -0500

    Adding porkbun ddclient

--- hosts/common/optional/services/ddclient.nix
+++ hosts/common/optional/services/ddclient.nix
@@ -4,8 +4,8 @@
   services.ddclient = {
     enable = true;
     ssl = true;
-    protocol = "googledomains";
-    username = "jIuresUnUC1pBaK1";
+    protocol = "porkbun";
+    username = "pk1_d2c1119ce79c7f8e82ce147518d741671ede87b6c12f88a8b582f14a2746a184";
     passwordFile = "${config.sops.secrets."ddclient-password".path}";
     domains = [ "home.${configVars.domain}" ];
   };
