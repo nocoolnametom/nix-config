538ca4d772a3c4c6788b2882f134d9ea4ea62d64
Tom Doggett <tom@nocoolnametom.com>
2024-11-18 09:16:32 -0500

    Moving more colors to Stylix

--- home/tdoggett/common/optional/desktops/hyprlock.nix
+++ home/tdoggett/common/optional/desktops/hyprlock.nix
@@ -16,7 +16,6 @@
         }
       ];
 
-      # TODO Can this be styled with Stylix?
       input-field = lib.mkDefault [
         {
           size = "200, 50";
@@ -24,11 +23,8 @@
           monitor = "";
           dots_center = true;
           fade_on_empty = false;
-          font_color = "rgb(202, 211, 245)";
-          inner_color = "rgb(91, 96, 120)";
-          outer_color = "rgb(24, 25, 38)";
           outline_thickness = 5;
-          placeholder_text = ''<span foreground="##cad3f5">Password...</span>'';
+          placeholder_text = "Password...";
           shadow_passes = 2;
         }
       ];
