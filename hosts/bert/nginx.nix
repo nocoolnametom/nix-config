628c91067d74c222b031e2ab7e5e132cd7baa6a1
Tom Doggett <tom@nocoolnametom.com>
2024-12-29 23:46:37 -0500

    Moving auth off to correct subdomain

--- hosts/bert/nginx.nix
+++ hosts/bert/nginx.nix
@@ -355,7 +355,6 @@
             proxyPass = "http://${configVars.networking.subnets.sauron.ip}:${builtins.toString configVars.networking.ports.tcp.invokeai}/";
             proxyWebsockets = true;
             extraConfig = ''
-              auth_basic off;
               proxy_buffering off;
               proxy_cache off;
               chunked_transfer_encoding off;
@@ -377,6 +376,7 @@
             proxyPass = "http://${configVars.networking.subnets.cirdan.ip}:${builtins.toString configVars.networking.ports.tcp.kavita}/";
             proxyWebsockets = true;
             extraConfig = ''
+              auth_basic off;
               proxy_set_header Host $host;
               proxy_set_header X-Real-IP $remote_addr;
               proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for; aio threads;
