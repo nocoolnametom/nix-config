1547a6998871803bcae49d28261ff159cfec6bd8
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2025-01-15 12:05:05 -0500

    Pinning darwin to same release as nixpkgs and home-manager

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-24.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
