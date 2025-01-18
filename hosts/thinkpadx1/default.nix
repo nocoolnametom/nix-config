8cec4a4ead7e932cc309bdcef8e06612686b68a1
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2025-01-17 23:53:15 -0500

    Fixing thinkpad error

--- hosts/thinkpadx1/default.nix
+++ hosts/thinkpadx1/default.nix
@@ -55,7 +55,7 @@
       "hosts/common/optional/stylix.nix" # System-wide styling
 
       #################### Users to Create ####################
-      "home/${configVars.username}/persistence/thinkpad11.nix"
+      "home/${configVars.username}/persistence/thinkpadx1.nix"
       "hosts/common/users/${configVars.username}"
     ]);
 
