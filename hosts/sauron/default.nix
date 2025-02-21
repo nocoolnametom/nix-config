2bf4206ff53c76ee954e8c175302357033028838
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2025-02-21 11:35:57 -0500

    Updating sauron llama models

--- hosts/sauron/default.nix
+++ hosts/sauron/default.nix
@@ -47,48 +47,38 @@
   services.ollama.loadModels = [
     # Models higher than 9GB will use CPU with GPU, less will be entirely on GPU
     # You CAN get a model up to ~40GB, but it'll mostly be CPU driven and slow.
+    #  4.9 GB  2025-02-19
+    "huihui_ai/deephermes3-abliterated:8b"
+    #  1.5 GB  2025-02-14
+    "huihui_ai/deepscaler-abliterated:1.5b"
     #  4.9 GB  2025-01-25
     "huihui_ai/deepseek-r1-abliterated:8b"
-    #  9.0 GB  2025-01-24
-    "huihui_ai/deepseek-r1-abliterated:14b"
     #  4.9 GB  2025-01-08
     "huihui_ai/dolphin3-abliterated:8b"
     #  9.1 GB  2025-01-09
     "huihui_ai/phi4-abliterated:14b" # Requires ollama 0.5.5+, which is why we're using unstable
     #  4.7 GB  2025-01-28
     "huihui_ai/qwen2.5-1m-abliterated:7b"
-    #  9.0 GB  2025-01-28
-    "huihui_ai/qwen2.5-1m-abliterated:14b"
     #  8.6 GB  2024-05-01
     "superdrew100/phi3-medium-abliterated"
     #  4.9 GB  2025-01-21
     "deepseek-r1:8b"
-    #  9.0 GB  2025-01-21
-    "deepseek-r1:14b"
     #  4.9 GB  2024-12-29
     "dolphin3:8b"
     #  4.7 GB  2024-05-20
     "dolphin-llama3:8b"
     #  4.1 GB  2024-03-01
     "dolphin-mistral:7b"
-    #  1.6 GB  2023-12-24
-    "dolphin-phi:2.7b"
     #  5.0 GB  2025-01-18
     "granite3.1-dense:8b"
     #  4.9 GB  2024-11-01
     "llama3.1:8b"
     #  2.0 GB  2024-09-01
     "llama3.2:3b"
-    #  4.1 GB  2023-12-12
-    "mistral:7b"
     #  7.1 GB  2024-07-18
     "mistral-nemo:12b"
     # 13.0 GB  2024-09-01
     "mistral-small:22b"
-    #  8.4 GB 2025-01-16
-    "olmo2:13b"
-    #  9.1 GB  2025-01-01
-    "phi4:14b"
   ];
   services.ollama.acceleration = "cuda";
   # The existing systemd job is SO tightened down that it can't read the WSL drivers AT ALL
