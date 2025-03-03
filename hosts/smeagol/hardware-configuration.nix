c6505010368142a219fb1a46129ded0baf80a134
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2025-03-03 16:12:55 -0500

    Fixing smeagol secrets

--- hosts/smeagol/hardware-configuration.nix
+++ hosts/smeagol/hardware-configuration.nix
@@ -13,11 +13,11 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-label/smeagol";
-      fsType = "btrfs";
-      options = [ "subvol=root" "compress=zstd" "noatime" ];
-    };
+  fileSystems."/" = {
+    device = "none";
+    fsType = "tmpfs";
+    options = [ "defaults" "size=25%" "mode=755" ];
+  };
 
   fileSystems."/home" =
     { device = "/dev/disk/by-label/smeagol";
@@ -37,6 +37,12 @@
       fsType = "btrfs";
       options = [ "subvol=persist" "compress=zstd" "noatime" ];
       neededForBoot = true;
+    };
+
+  fileSystems."/etc/nixos" =
+    { device = "/dev/disk/by-label/smeagol";
+      fsType = "btrfs";
+      options = [ "subvol=nixos-config" "compress=zstd" "noatime" ];
     };
 
   fileSystems."/var/log" =
@@ -64,44 +70,4 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-  boot.initrd.enable = true;
-  boot.supportedFilesystems = [ "btrfs" ];
-  boot.initrd.postResumeCommands = lib.mkAfter ''
-    mkdir -p /mnt
-    # We first mount the btrfs root to /mnt
-    # so we can manipulate btrfs subvolumes.
-    mount -o subvol=/ /dev/disk/by-label/smeagol /mnt
-
-    # While we're tempted to just delte /root and create
-    # a new snapshot from /root-black, /root is already
-    # populated at this point with a number of subvolumes,
-    # which makes `btrfs subvolume delete` fail.
-    # So, we remove them first.
-    # 
-    # /root contains subvolumes:
-    # - /root/var/lib/portables
-    # - /root/var/lib/machines
-    # 
-    # I suspecte these are related to systemd-nspawn, but
-    # since I don't use it I'm not 100% sure.
-    # Anyhow, deleting these subvolumes hasn't resulted
-    # in any issues so far, except for fairly
-    # benign-looking error from systemd-tmpfiles.
-    btrfs subvolume list -o /mnt/root |
-    cut -f9 -d ' ' |
-    while read subvolume; do
-      echo "deleting /$subvolume subvolume..."
-      btrfs subvolume delete "/mnt/$subvolume"
-    done &&
-    echo "deleting /root subvolume..." &&
-    btrfs subvolume delete /mnt/root
-
-    echo "restoring blank /root subvolume..."
-    btrfs subvolume snapshot /mnt/root-blank /mnt/root
-
-    # Once we're done rolling back to a blank snapshot,
-    # we can unmount /mnt and continue on the boot process.
-    umount /mnt
-  '';
 }
