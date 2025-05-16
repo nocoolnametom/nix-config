5337e7b8085f0e571eeb05b9e20a2b952d956674
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2025-05-16 17:16:02 -0400

    Cleanup determinate config

--- hosts/common/core/nix.nix
+++ hosts/common/core/nix.nix
@@ -40,7 +40,7 @@
   };
 
   # Needed since Determinate Nix manages the main config file for system.
-  environment.etc."nix/nix.custom.conf".text = pkgs.lib.mkForce ''
+  environment.etc."nix/nix.custom.conf".text = lib.mkDefault ''
     # Written by https://github.com/DeterminateSystems/nix-installer.
     # The contents below are based on options specified at installation time.
     trusted-users = ${configVars.username}
