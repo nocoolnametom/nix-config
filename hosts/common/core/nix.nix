78adcbb87be62bae013acb5fec7f0e3c335121b4
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2025-05-16 17:14:26 -0400

    Adding determiate config to repo

--- hosts/common/core/nix.nix
+++ hosts/common/core/nix.nix
@@ -38,4 +38,12 @@
     };
 
   };
+
+  # Needed since Determinate Nix manages the main config file for system.
+  environment.etc."nix/nix.custom.conf".text = pkgs.lib.mkForce ''
+    # Written by https://github.com/DeterminateSystems/nix-installer.
+    # The contents below are based on options specified at installation time.
+    trusted-users = ${configVars.username}
+    lazy-trees = true
+  '';
 }
