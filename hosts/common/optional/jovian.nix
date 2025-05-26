1c517f3e0e66b00a10a1f384cd22e688be1f7d2a
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2025-05-26 09:16:10 -0400

    Fixing jovian config

--- hosts/common/optional/jovian.nix
+++ hosts/common/optional/jovian.nix
@@ -17,9 +17,17 @@
 
   environment.sessionVariables.NIXOS_OZONE_WL = lib.mkDefault "1";
 
-  jovian.steam.environment = {} // (lib.optional ((lib.attrsets.hasAttrByPath ["services" "wivrn" "enable"] config) && (config.services.wivrn.enable)) {
-    PRESSURE_VESSEL_FILESYSTEMS_RW = "$XDG_RUNTIME_DIR/wivrn/comp_ipc";
-  });
+  jovian.steam.environment =
+    { }
+    // (lib.attrsets.optionalAttrs
+      (
+        (lib.attrsets.hasAttrByPath [ "services" "wivrn" "enable" ] config)
+        && (config.services.wivrn.enable)
+      )
+      {
+        PRESSURE_VESSEL_FILESYSTEMS_RW = "$XDG_RUNTIME_DIR/wivrn/comp_ipc";
+      }
+    );
 
   services.desktopManager.plasma6.enable = true;
   services.xserver.enable = false;
