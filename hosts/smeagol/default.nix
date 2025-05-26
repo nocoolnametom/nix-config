158acaf61d38bb0198c57034c7cb01a2f3026353
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2025-05-26 10:58:24 -0400

    Turning off suspend for smeagol

--- hosts/smeagol/default.nix
+++ hosts/smeagol/default.nix
@@ -98,6 +98,7 @@
   # Some Jovian stuff just for smeagol
   jovian.devices.steamdeck.enable = lib.mkForce false;
   jovian.hardware.has.amd.gpu = lib.mkForce false;
+  systemd.targets.suspend.enable = lib.mkForce false;
 
   # The networking hostname is used in a lot of places, such as secret retrieval!
   networking = {
