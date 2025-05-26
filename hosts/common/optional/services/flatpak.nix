906fcc4393a48c500b76d8ff0cef840b4c5d93ef
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2025-05-26 07:53:51 -0400

    Updated smeagol services

--- hosts/common/optional/services/flatpak.nix
+++ hosts/common/optional/services/flatpak.nix
@@ -1,6 +1,8 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
   imports = [ inputs.nix-flatpak.nixosModules.nix-flatpak ];
 
   services.flatpak.enable = true;
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 }
