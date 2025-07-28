64fac77c9bfb7dd586f00cf95ac305f6308782e0
Tom Doggett <tom@nocoolnametom.com>
2025-07-28 11:24:27 -0400

    Enabling SSO for Navidrome

--- hosts/common/optional/services/navidrome.nix
+++ hosts/common/optional/services/navidrome.nix
@@ -1,12 +1,10 @@
-{ ... }:
+{ lib, configVars, ... }:
 {
   # Navidrome Music Server
-  services.navidrome.enable = true;
-  services.navidrome.settings = {
-    Address = "0.0.0.0";
-    Port = 4533;
-    BaseUrl = "/music";
-    # MusicFolder = "/mnt/Backup/Takeout/${configVars.handle}/Google_Play_Music";
-    DataFolder = "/var/lib/navidrome";
-  };
+  services.navidrome.enable = lib.mkDefault true;
+  services.navidrome.settings.Address = lib.mkDefault "0.0.0.0";
+  services.navidrome.settings.Port = lib.mkDefault 4533;
+  services.navidrome.settings.BaseUrl = lib.mkDefault "/music";
+  services.navidrome.settings.MusicFolder = lib.mkDefault "/mnt/Backup/Takeout/${configVars.handle}/Google_Play_Music";
+  services.navidrome.settings.DataFolder = lib.mkDefault "/var/lib/navidrome";
 }
