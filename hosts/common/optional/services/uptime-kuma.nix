6839835542e5f85a3f02f4067d9fcd605cf6eb65
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2025-07-31 12:02:30 -0400

    Replace netbox with uptime-kuma monitoring on bombadil

---
+++ hosts/common/optional/services/uptime-kuma.nix
@@ -0,0 +1,4 @@
+{ lib, configVars, ... }: {
+  services.uptime-kuma.enable = lib.mkDefault true;
+  services.uptime-kuma.settings.PORT = builtins.toString configVars.networking.ports.tcp.uptime-kuma;
+}
