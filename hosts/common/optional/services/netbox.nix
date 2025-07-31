0107d979a507ab92c21749ce152ef42668fab48a
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2025-07-31 12:13:13 -0400

    Netbox plugins

--- hosts/common/optional/services/netbox.nix
+++ hosts/common/optional/services/netbox.nix
@@ -3,4 +3,14 @@
   sops.secrets."netbox-keyfile".owner = "netbox";
   services.netbox.enable = lib.mkDefault true;
   services.netbox.secretKeyFile = config.sops.secrets."netbox-keyfile".path;
+  services.netbox.plugins = ps: with ps; [
+    ps.netbox-bgp
+    ps.netbox-documents
+    ps.netbox-reorder-rack
+  ];
+  services.netbox.settings.PLUGINS = [
+    "netbox_bgp"
+    "netbox_documents"
+    "netbox_reorder_rack"
+  ];
 }
