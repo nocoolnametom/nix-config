ea7ba4c3a0920177e0939ed4cd5fee832bd0db50
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2025-08-04 10:08:25 -0400

    nh directory path uses my handle instead of username

--- home/tdoggett/common/core/nh.nix
+++ home/tdoggett/common/core/nh.nix
@@ -1,5 +1,5 @@
 { lib, config, configVars, ... }: {
   programs.nh.enable = lib.mkDefault true;
   programs.nh.clean.enable = lib.mkDefault true;
-  programs.nh.flake = lib.mkDefault "${config.home.homeDirectory}/Projects/${configVars.username}/nix-config";
+  programs.nh.flake = lib.mkDefault "${config.home.homeDirectory}/Projects/${configVars.handle}/nix-config";
 }
