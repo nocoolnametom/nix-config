3d3e90e9f0c10f87de682d19f2b3767024dacf8a
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2025-08-30 22:38:08 -0400

    Authing pinchflat

--- hosts/common/optional/services/pinchflat.nix
+++ hosts/common/optional/services/pinchflat.nix
@@ -6,6 +6,8 @@
 }:
 {
   sops.secrets."pinchflat/key-base" = { };
+  sops.secrets."pinchflat/username" = { };
+  sops.secrets."pinchflat/password" = { };
   services.pinchflat.enable = lib.mkDefault true;
   services.pinchflat.openFirewall = lib.mkDefault true;
   services.pinchflat.port = lib.mkDefault configVars.networking.ports.tcp.pinchflat;
@@ -17,6 +19,8 @@
   sops.templates."pinchflat-secrets.env" = {
     content = ''
       SECRET_KEY_BASE=${config.sops.placeholder."pinchflat/key-base"}
+      BASIC_AUTH_USERNAME=${config.sops.placeholder."pinchflat/username"}
+      BASIC_AUTH_PASSWORD=${config.sops.placeholder."pinchflat/password"}
     '';
     owner =
       if (builtins.hasAttr "user" config.services.pinchflat) then
