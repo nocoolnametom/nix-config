4d7a6e2ecc28a784953049670429ddc3dcaf6c60
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2025-09-19 16:20:05 -0400

    extra-substituters should be present through using core/nix.nix

--- hosts/common/core/default.nix
+++ hosts/common/core/default.nix
@@ -21,20 +21,6 @@
       # Custom nixosModules as defined in the root flake
       (builtins.attrValues outputs.nixosModules);
 
-  # Use the Community and Hyprland Cache
-  nix.settings.substituters = [
-    "https://hyprland.cachix.org"
-    "https://nix-community.cachix.org"
-  ];
-  nix.settings.trusted-substituters = [
-    "https://hyprland.cachix.org"
-    "https://nix-community.cachix.org"
-  ];
-  nix.settings.trusted-public-keys = [
-    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-  ];
-
   # Deduplicate and optimize nix store
   nix.optimise.automatic = true;
 
--- hosts/common/core/nix.nix
+++ hosts/common/core/nix.nix
@@ -34,10 +34,26 @@
         "flakes"
       ];
 
-      extra-substituters = [
+      # Use the Community Caches
+      substituters = [
+        "https://ai.cachix.org"
+        "https://cuda-maintainers.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
         "https://nixos-raspberrypi.cachix.org"
       ];
-      extra-trusted-public-keys = [
+      trusted-substituters = [
+        "https://ai.cachix.org"
+        "https://cuda-maintainers.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://nixos-raspberrypi.cachix.org"
+      ];
+      trusted-public-keys = [
+        "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
       ];
       trusted-users = [
