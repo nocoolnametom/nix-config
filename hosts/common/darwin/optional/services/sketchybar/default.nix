5fa8a56cc45de97110a2a505dc425516bfae88ef
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2025-10-09 14:29:06 -0400

    Removing audio from sketchybar as the monitor speakers don't report their levels

--- hosts/common/darwin/optional/services/sketchybar/default.nix
+++ hosts/common/darwin/optional/services/sketchybar/default.nix
@@ -122,9 +122,6 @@ in
 
     sketchybar --add item clock right \
               --set clock update_freq=10 icon=  script="${plugins.clock}" \
-              --add item volume right \
-              --set volume script="${plugins.volume}" \
-              --subscribe volume volume_change \
               --add item battery right \
               --set battery update_freq=120 script="${plugins.battery}" \
               --subscribe battery system_woke power_source_change \
