18dd283e2dcbc7eb871c4a1b23a2f572ef737af8
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2025-10-24 16:22:15 -0400

    Have ombi use the unstable pkg

--- hosts/common/optional/services/ombi.nix
+++ hosts/common/optional/services/ombi.nix
@@ -1,8 +1,9 @@
-{
+{ pkgs, lib, ... }: {
   # Ombi Requesting Server
-  services.ombi.enable = true;
+  services.ombi.enable = lib.mkDefault true;
+  services.ombi.package = lib.mkDefault pkgs.unstable.ombi;
 
-  services.postgresql.enable = true;
+  services.postgresql.enable = lib.mkDefault true;
   services.postgresql.ensureDatabases = [ "ombi" ];
   services.postgresql.ensureUsers = [
     {
