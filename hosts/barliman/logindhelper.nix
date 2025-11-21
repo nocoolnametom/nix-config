a6cc47320e67cba2dce4e2d2cb9cc3f0f4945268
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2025-11-21 15:06:27 -0500

    jovian login helper

--- hosts/barliman/logindhelper.nix
+++ hosts/barliman/logindhelper.nix
@@ -2,7 +2,7 @@
 {
   # As of 2025-10-24 Jovian is trying to set this option that is no longer present
   options.services.logind.settings.Login.HandlePowerKey = lib.mkOption {
-    type = lib.types.string;
+    type = lib.types.str;
     default = "";
   };
 }
