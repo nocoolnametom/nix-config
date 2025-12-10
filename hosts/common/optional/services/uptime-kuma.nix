501e5f552655df23457ed2a3e92c6c8b0c407737
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2025-12-10 06:32:05 -0600

    typo

--- hosts/common/optional/services/uptime-kuma.nix
+++ hosts/common/optional/services/uptime-kuma.nix
@@ -1,7 +1,7 @@
 { lib, pkgs, configVars, ... }:
 {
   services.uptime-kuma.enable = lib.mkDefault true;
-  services.uptime-kuma.package = lib.mkDefault pkgs.unstale.uptime-kuma;
+  services.uptime-kuma.package = lib.mkDefault pkgs.unstable.uptime-kuma;
   services.uptime-kuma.settings.PORT = builtins.toString configVars.networking.ports.tcp.uptime-kuma;
   services.uptime-kuma.settings.UPTIME_KUMA_DB_TYPE = lib.mkDefault "sqlite";
 }
