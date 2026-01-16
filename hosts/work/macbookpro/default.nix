1c35219cec6831fcb23b344e6e065eefe6ad8182
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2026-01-16 12:04:34 -0500

    Updating nixpkgs for macbook

--- hosts/work/macbookpro/default.nix
+++ hosts/work/macbookpro/default.nix
@@ -15,7 +15,17 @@
   configurationRevision,
   ...
 }:
-{
+
+let
+  externallyManagedPackageNames = [
+    # These are kept MORE up-to-date than unstable usually allows
+    # so we need to defer to using the system services instead of
+    # Nix-Darwin to handle them.
+    "google-chrome"
+    "google-chrome-beta"
+    "google-chrome-canary"
+  ];
+in {
   imports = [
     ######################## Direct Imports for MBP ############################
   ]
@@ -56,25 +66,29 @@
   # makes sense to not have a useless service just sitting open
   # services.synergy.server.enable = false;
 
+  # Remember to use unstable for packages that should be kept up-to-date.
+  # If the most recent unstable package version can't satisfy the work auto-updater
+  # be certain to add the package name to the list of
+  # `externallyManagedPackageNames` at the top of this file and remove it from this
+  # list and use the system services to keep it updated for you.
   environment.systemPackages = [
     # Graphical Programs
-    pkgs.code-cursor
-    pkgs.google-chrome
-    pkgs.iterm2
-    pkgs.obsidian
-    pkgs.podman-desktop
-    pkgs.postman
-    pkgs.protonmail-bridge
-    pkgs.protonmail-desktop
-    pkgs.sequelpro
-    pkgs.slack
-    pkgs.tableplus
-    pkgs.vscode
-    pkgs.zed-editor
-    pkgs.zoom-us
+    pkgs.unstable.code-cursor
+    pkgs.unstable.iterm2
+    pkgs.unstable.obsidian
+    pkgs.unstable.podman-desktop
+    pkgs.unstable.postman
+    pkgs.unstable.protonmail-bridge
+    pkgs.unstable.protonmail-desktop
+    pkgs.unstable.sequelpro
+    pkgs.unstable.slack
+    pkgs.unstable.tableplus
+    pkgs.unstable.vscode
+    pkgs.unstable.zed-editor
+    pkgs.unstable.zoom-us
 
     # Terminal Programs
-    pkgs.claude-code
+    pkgs.unstable.claude-code
     pkgs.awscli2
     pkgs.kubectl # same as kubernetes-cli
     pkgs.libaom
@@ -84,6 +98,19 @@
     pkgs.openjdk11 # Why do I need version 11? What is this for?
     pkgs.uv # Python Env/Pkg Manager
   ];
+  assertions = let
+    forbidden = lib.filter (pkg: lib.elem (lib.getName pkg) externallyManagedPackageNames) config.environment.systemPackages;
+  in [{
+    assertion = forbidden == [];
+    message = ''
+      These packages are managed by work services and kept up to date FAR sooner than
+      nixpkgs-unstable allows, so we cannot install them via Nix so that the security
+      of this system remains appropriate to what its owners demand.  You must remove
+      the following packages from environment.systemPackages:
+
+      ${lib.concatMapStringsSep "\n" lib.getName forbidden}
+    '';
+  }];
 
   # There is no system-level vim management for NixOS, only darwin
   # TODO: See if I can move this into home-manager instead
