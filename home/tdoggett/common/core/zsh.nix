7b4dc406355e6858c13b57211c3fc5dae2d6fcbd
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2026-03-07 15:58:34 -0500

    Fixing stylix warnings

--- home/tdoggett/common/core/zsh.nix
+++ home/tdoggett/common/core/zsh.nix
@@ -10,7 +10,7 @@
   # Force SSH_AUTH_SOCK to GPG agent (overrides COSMIC session environment)
   # This is needed because COSMIC starts gnome-keyring with --login which sets SSH_AUTH_SOCK
   # before Home Manager's sessionVariables are applied
-  programs.zsh.initExtra = ''
+  programs.zsh.initContent = ''
     # Use GPG agent for SSH authentication
     export SSH_AUTH_SOCK="''${XDG_RUNTIME_DIR:-/run/user/$UID}/gnupg/S.gpg-agent.ssh"
   '';
--- home/tdoggett/common/optional/desktops/firefox.nix
+++ home/tdoggett/common/optional/desktops/firefox.nix
@@ -1,4 +1,16 @@
 { lib, ... }:
 {
-  programs.firefox.enable = lib.mkDefault true;
+  programs.firefox = {
+    enable = lib.mkDefault true;
+
+    # Define a default profile for Stylix to theme
+    profiles.default = {
+      id = 0;
+      name = "default";
+      isDefault = true;
+    };
+  };
+
+  # Tell Stylix which profile(s) to theme (fixes build warning)
+  stylix.targets.firefox.profileNames = [ "default" ];
 }
