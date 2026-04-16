4d11687de827fd5da493beff251f19e7e095c401
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2026-04-16 18:18:29 -0400

    Fixing some 25.11 -> 26.05 changes

--- hosts/bombadil/hardware-configuration.nix
+++ hosts/bombadil/hardware-configuration.nix
@@ -123,43 +123,33 @@
 
   boot.initrd.enable = true;
   boot.supportedFilesystems = [ "btrfs" ];
-  boot.initrd.postResumeCommands = lib.mkAfter ''
-    mkdir -p /mnt
-    # We first mount the btrfs root to /mnt
-    # so we can manipulate btrfs subvolumes.
-    mount -o subvol=/ /dev/disk/by-label/nixos /mnt
+  boot.initrd.systemd.services.rollback-root = {
+    description = "Rollback root filesystem to blank snapshot";
+    wantedBy = [ "initrd.target" ];
+    after = [ "initrd-root-device.target" ];
+    before = [ "sysroot.mount" ];
+    unitConfig.DefaultDependencies = "no";
+    serviceConfig.Type = "oneshot";
+    script = ''
+      mkdir -p /mnt
+      mount -o subvol=/ /dev/disk/by-label/nixos /mnt
 
-    # While we're tempted to just delte /root and create
-    # a new snapshot from /root-black, /root is already
-    # populated at this point with a number of subvolumes,
-    # which makes `btrfs subvolume delete` fail.
-    # So, we remove them first.
-    # 
-    # /root contains subvolumes:
-    # - /root/var/lib/portables
-    # - /root/var/lib/machines
-    # 
-    # I suspecte these are related to systemd-nspawn, but
-    # since I don't use it I'm not 100% sure.
-    # Anyhow, deleting these subvolumes hasn't resulted
-    # in any issues so far, except for fairly
-    # benign-looking error from systemd-tmpfiles.
-    btrfs subvolume list -o /mnt/root |
-    cut -f9 -d ' ' |
-    while read subvolume; do
-      echo "deleting /$subvolume subvolume..."
-      btrfs subvolume delete "/mnt/$subvolume"
-    done &&
-    echo "deleting /root subvolume..." &&
-    btrfs subvolume delete /mnt/root
+      btrfs subvolume list -o /mnt/root |
+      cut -f9 -d ' ' |
+      while read subvolume; do
+        echo "deleting /$subvolume subvolume..."
+        btrfs subvolume delete "/mnt/$subvolume"
+      done
 
-    echo "restoring blank /root subvolume..."
-    btrfs subvolume snapshot /mnt/root-blank /mnt/root
+      echo "deleting /root subvolume..."
+      btrfs subvolume delete /mnt/root
 
-    # Once we're done rolling back to a blank snapshot,
-    # we can unmount /mnt and continue on the boot process.
-    umount /mnt
-  '';
+      echo "restoring blank /root subvolume..."
+      btrfs subvolume snapshot /mnt/root-blank /mnt/root
+
+      umount /mnt
+    '';
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
