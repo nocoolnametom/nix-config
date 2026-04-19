741235b629ce086980f037bc774d54354927c45e
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2026-04-19 12:30:59 -0400

    Fixing thunar warnings

--- hosts/common/optional/thunar.nix
+++ hosts/common/optional/thunar.nix
@@ -2,8 +2,8 @@
 {
   programs.thunar.enable = lib.mkDefault true;
   programs.thunar.plugins = [
-    pkgs.xfce.thunar-archive-plugin
-    pkgs.xfce.thunar-volman
+    pkgs.thunar-archive-plugin
+    pkgs.thunar-volman
   ];
   services.gvfs.enable = lib.mkDefault true; # Mount, trash, and other functionalities
   services.tumbler.enable = lib.mkDefault true; # Thumbnail support for images
