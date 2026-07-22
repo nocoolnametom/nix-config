52b094aeb5015057ec3e487b227fa750fded5d7c
Tom Doggett <810877+nocoolnametom@users.noreply.github.com>
2026-07-22 14:43:53 -0400

    Fix atuin server host

--- hosts/common/optional/services/atuin.nix
+++ hosts/common/optional/services/atuin.nix
@@ -5,7 +5,7 @@
 }:
 {
   services.atuin.enable = lib.mkDefault true;
-  services.atuin.host = lib.mkDefault "127.0.0.1";
+  services.atuin.host = lib.mkDefault "0.0.0.0";
   services.atuin.port = lib.mkDefault configVars.networking.ports.tcp."atuin-sync";
   services.atuin.openRegistration = lib.mkDefault false;
   services.atuin.database.createLocally = lib.mkDefault true;
